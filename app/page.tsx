121f4d471e6883fe923055b2b04b594865624152
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-06-21 21:21:14 +0100

    Styled the form section.

--- app/page.tsx
+++ app/page.tsx
@@ -1,84 +1,66 @@
 "use client"
 import Image from "next/image";
 import bgDesktop from "../public/images/bgDesktop.svg"
-import type { FormProps } from 'antd';
-import { Button, Checkbox, Form, Input } from 'antd';
+import "./App.css"
 
 
-
-type FieldType = {
-  username?: string;
-  password?: string;
-  remember?: string;
-};
-
-const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-  console.log('Success:', values);
-};
-
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
 
 
 export default function Home() {
   return (
-    <div className=" bg-[#f0f6ff] flex min-h-screen flex-col items-center justify-between p-24">
-      <main className="p-5 md:rounded md:bg-white md:shadow-lg w-10/12 ">
+    <div className=" bg-[#f0f6ff] relative flex min-h-screen flex-col items-center justify-between p-8">
+      <main className="p-5 md:rounded md:bg-white md:shadow-lg w-9/12 ">
         <div className="flex flex-col gap-4 md:flex-row">
           {/* for mobile */}
           {/* <div className="md:hidden w-full h-[172px] absolute top-0 bg-mobile left-0 bg-cover -z-10">
 
               </div> */}
           {/* sidebar */}
-          <div className="bg-desktop bg-cover">
-            <Image src={bgDesktop} alt="desktop background" />
+          <div className="sidebar md:bg-[url('/images/bgDesktop.svg')] bg-cover w-[274px] h-[568px] roundex-xl hidden md:block">
+            <div className="grid gap-6 p-10"></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
-          {/* form */}
+          {/* form container */}
           <div className="rounded-lg shadow-lg md:shadow-none z-10 flex-1 p-5 bg-white ">
-            <Form
-              name="basic"
-              labelCol={{ span: 8 }}
-              wrapperCol={{ span: 16 }}
-              style={{ maxWidth: 600 }}
-              initialValues={{ remember: true }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-              autoComplete="off"
-              layout="vertical"
-            >
-              <h1>Personal Info</h1>
-              <p>Please provide your name, email address, and phone number.</p>
-              <Form.Item<FieldType>
-                label="Name"
-                name="username"
-                rules={[{ required: true, message: 'e.g. Stephen King' }]}
-              >
-                <Input placeholder="e.g. Stephen King" />
-              </Form.Item>
+            <div className="flex flex-col md:shadow-none gap-4 mx-auto h-full md:w-4/5">
+              <div>
+                <h1 className="font-bold text-3xl text-[#02295A]">Personal Info</h1>
+                <p className="text-[#9699AB] text-base">Please provide your name, email address, and phone number.</p>
+              </div>
+              <div className="hidden"></div>
+              <div className="hidden"></div>
+              <div className="hidden"></div>
+              {/* form */}
 
-              <Form.Item<FieldType>
-                label="Email Address"
-                // name="email"
-                rules={[{ required: true, message: 'e.g. Stephen King' }]}
-              >
-                <Input placeholder="e.g. stephenking@lorem.com" />
-              </Form.Item>
-
-              <Form.Item<FieldType>
-                label="Phone Number"
-                // name="phone"
-                rules={[{ required: true, message: 'e.g. Stephen King' }]}
-              >
-                <Input placeholder="e.g. +1 234 567 890" />
-              </Form.Item>
-
-              <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <Button type="primary" htmlType="submit">
-                  Next Step
-                </Button>
-              </Form.Item>
-            </Form>
+              <form action="onSubmit" className="grid gap-4">
+                {/* <div className="grid"> */}
+                <div className="grid ">
+                  <label htmlFor="Name">Name
+                  </label>
+                  <input className="mt-[.25rem] border-2 border-[#d6d9e6] w-full py-2 px-3 rounded" type="text" placeholder="e.g. Stephen King" />
+                </div>
+                <div className="grid">
+                  <label htmlFor="Name">Phone Number
+                  </label>
+                  <input className="mt-[.25rem] border-2 border-[#d6d9e6] w-full py-2 px-3 rounded" type="text" placeholder="e.g. stephenking@lorem.com" />
+                </div>
+                <div className="grid">
+                  <label htmlFor="Name">Email Address
+                  </label>
+                  <input className="mt-[.25rem] border-2 border-[#d6d9e6] w-full py-2 px-3 rounded" type="text" placeholder="e.g. +1 234 567 890" />
+                </div>
+                <div className="hidden md:flex justify-between mt-[146px] bg-red-500">
+                  <button className="hidden">Go Back</button>
+                  <button className="bg-[#02295A] py-3 px-5 rounded-lg text-white ml-auto duration-[.15s] cursor-pointer" type="submit">
+                    Next Step
+                  </button>
+                </div>
+                {/* </div> */}
+              </form>
+            </div>
           </div>
         </div>
       </main>
