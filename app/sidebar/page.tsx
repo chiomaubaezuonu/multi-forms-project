2164fd504aa4d40c4df126b5bf5b4506bcbccbf1
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-07-16 13:57:02 +0100

    Reduced the width of the sidebar

--- app/sidebar/page.tsx
+++ app/sidebar/page.tsx
@@ -19,7 +19,7 @@ const sideLinks = [
     linkNum: 1,
     stepNum: "Step 1",
     details: "Your info",
-    href: '/' 
+    href: '/'
   },
   {
     id: 2,
@@ -33,14 +33,14 @@ const sideLinks = [
     linkNum: 3,
     stepNum: "Step 3",
     details: "Add-Ons",
-     href: '/add-ons'
+    href: '/add-ons'
   },
   {
     id: 4,
     linkNum: 4,
     stepNum: "Step 4",
     details: "Summary",
-      href: '/summary'
+    href: '/summary'
   }
 ]
 
@@ -59,10 +59,10 @@ const Sidebar = () => {
   const [links, setLinks] = useState<Sidebarlinks>()
   const [activePage, setActivepage] = useState()
 
-  
+
   return (
     <div>
-      <div className="sidebar bg-[url('/images/bg-sidebar-mobile.svg')] md:bg-[url('/images/bgDesktop.svg')] bg-cover w-full md:w-[274px] h-[172px] md:h-[538px] md:rounded-xl">
+      <div className="sidebar bg-[url('/images/bg-sidebar-mobile.svg')] mr-4 md:bg-[url('/images/bg-sidebar-desktop.svg')] bg-cover w-full md:w-[264px] h-[172px] md:h-[538px] md:rounded-xl">
         <div className="flex justify-center gap-4 py-5 z-5  md:grid md:gap-6 md:p-10">
           {/* <div className="flex justify-center gap-4 py-5 z-5 md:hidden"> */}
 
@@ -73,7 +73,7 @@ const Sidebar = () => {
 
               return <div key={sideLink.id}>
                 {/* <Link href='/plan'> */}
-                <div className="flex gap-4 items-center cursor-pointer">
+                <div className="flex gap-4 items-center cursor-pointer onClick={handleNavigation(id)}">
                   <div className={`${isActive ? 'bg-[#BFE2FD]' : ''} text-[#02295A] py-1 px-[0.628rem] border-[1px] text-sm font-bold rounded-full border-white`}>{sideLink.linkNum}</div>
                   <div className="hidden md:block uppercase text-sm">
                     <p className="font-light text-xs text-[#ADBEFF]">{sideLink.stepNum}</p>
@@ -84,56 +84,7 @@ const Sidebar = () => {
               </div>
             })
           }
-
-
-
-          {/* <div onClick={() => setActiveColor(true)}>
-              <Link href='/' >
-                <div className="flex gap-4 items-center cursor-pointer">
-                  <div className={`text-[#02295A] py-1 px-[0.628rem] border-[1px] text-sm font-bold rounded-full border-white ${activeColor ? 'bg-[#BFE2FD]' : " "}`}>1</div>
-                  <div className="hidden md:block uppercase text-sm">
-                    <p className="font-light text-xs text-[#ADBEFF]">Step 1</p>
-                    <p className="tracking-widest text-[#F0F6FF] font-bold">Your info</p>
-                  </div>
-                </div>
-              </Link>
-            </div> */}
-          {/* </div> */}
-          {/* <div>
-            <Link href='/plan'>
-              <div className="flex gap-4 items-center cursor-pointer">
-                <div className="text-[#02295A] py-1 px-[0.628rem] border-[1px] text-sm font-bold rounded-full border-white">2</div>
-                <div className="hidden md:block uppercase text-sm">
-                  <p className="font-light text-xs text-[#ADBEFF]">Step 2</p>
-                  <p className="tracking-widest text-[#F0F6FF] font-bold">Select Plan</p>
-                </div>
-              </div>
-            </Link>
-          </div> */}
-          {/* <div>
-            <Link href='/add-ons'>
-              <div className="flex gap-4 items-center cursor-pointer">
-                <div className="text-[#02295A] py-1 px-[0.628rem] border-[1px] text-sm font-bold rounded-full border-white">3</div>
-                <div className="hidden md:block uppercase text-sm">
-                  <p className="font-light text-xs text-[#ADBEFF]">Step 3</p>
-                  <p className="tracking-widest text-[#F0F6FF] font-bold">add-ons</p>
-                </div>
-              </div>
-            </Link>
-          </div> */}
-          {/* <div>
-            <Link href='/summary'>
-              <div className="flex gap-4 items-center cursor-pointer">
-                <div className="text-[#02295A] py-1 px-[0.628rem] border-[1px] text-sm font-bold rounded-full border-white">4</div>
-                <div className="hidden md:block uppercase text-sm">
-                  <p className="font-light text-xs text-[#ADBEFF]">Step 4</p>
-                  <p className="tracking-widest text-[#F0F6FF] font-bold">summary</p>
-                </div>
-              </div>
-            </Link>
-          </div> */}
         </div>
-        {/* </div> */}
       </div>
     </div >
   )
