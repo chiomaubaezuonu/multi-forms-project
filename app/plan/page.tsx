36d1a578d0d19533e3b05874d651cd46a664ff44
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-07-20 22:29:25 +0100

    Made plan page mobile responsive

--- app/plan/page.tsx
+++ app/plan/page.tsx
@@ -95,27 +95,29 @@ const Plan = () => {
     // }
 
     return (
-        <div className="rounded-lg shadow-lg md:shadow-none z-10 flex-1 p-5 bg-white  absolute top-40 md:top-0 w-80 md:relative ">
-            <div className="flex flex-col md:shadow-none gap-4 mx-auto h-full md:w-4/5">
+        // <div className="rounded-lg shadow-lg md:shadow-none z-10 flex-1 p-5 bg-white  absolute top-40 md:top-0 w-80 md:relative ">
+        //     <div className="flex flex-col md:shadow-none gap-4 mx-auto h-full md:w-4/5">
+        <div className="flex flex-col md:mt-0 bg-yellow z-20 absolute md:relative top-40 md:top-0">
+      <div className="flex flex-col px-6 w-11/12 rounded-xl md:rounded-none md:px-8 mx-auto pt-4 pb-4 md:w-full bg-white">
                 <div>
-                    <h1 className="font-bold text-2xl mt-[2.063] md:text-3xl text-[#02295A]">Select your plan</h1>
-                    <p className="text-[#9699AB] mt-[0.688rem] text-base">You have the option of monthly or yearly billing.</p>
+                    <h1 className="font-bold text-2xl mt-[2.063] md:mb-2 pt-10 pb-4 px-2 md:px-6 md:p-0 md:text-3xl text-[#02295A]">Select your plan</h1>
+                    <p className="text-[#9699AB] mt-[0.688rem] text-base bg-white px-0 md:px-0 pr-14 md:pr-4  tracking-tight mb-4 md:mb-8 md:font-medium">You have the option of monthly or yearly billing.</p>
                 </div>
-                <div className='grid md:grid-cols-3 gap-4'>
+                <div className='grid md:grid-cols-3  gap-4'>
                     {
                         plans.map((plan, index) => (
-                            <div key={index} onClick={() => handlePlan(index)} className={`bg-[#473dff0d]  ${clickedPlan === index ? 'border-[#473dff]' : 'border-[#d6d9e6]'} hover:border-[#473dff] border-[0.0623rem] text-sm rounded-lg w-full border-[0.063] cursor-pointer flex md:flex-col h-[5.75rem] py-3 md:h-[11rem] p-4 gap-3 duration-200`}>
-                                <Image width={50} height={50} src={plan.image} alt='arcade' />
-                                <p className='text-[#0229A] font-bold text-lg mt-6'>{plan.plan}</p>
-                                <div className='mt-auto'>
+                            <div key={index} onClick={() => handlePlan(index)} className={`bg-[#473dff0d]  ${clickedPlan === index ? 'border-[#473dff]' : 'border-[#d6d9e6]'} hover:border-[#473dff] border-[0.0623rem] text-sm rounded-lg w-full md:w-[8.166rem] border-[0.063] cursor-pointer flex md:flex-col px-[0.688rem] py-[0.8rem] gap-3 duration-200`}>
+                                <Image width={40} height={40} src={plan.image} alt='arcade' className='mb-4' />
+                                <p className='text-[#02295A] font-semibold text-base '>{plan.plan}</p>
+                                <div className='mt-0'>
                                     {yearly ?
                                         <div>
-                                            <p className='text-sm text-[#9699ab]'>{plan.yearlyPrice}</p>
+                                            <p className='text-sm text-[#9699ab] font-medium'>{plan.yearlyPrice}/yr</p>
                                             <p className="text-[02295A] text-sm">{plan.duration}</p>
                                         </div>
                                         :
                                         <div>
-                                            <p className='text-[#9699ab] text-sm'>{plan.monthlyPrice}</p>
+                                            <p className='text-[#9699ab] font-medium text-sm'>{plan.monthlyPrice}/mo</p>
                                         </div>
                                     }
                                 </div>
@@ -125,13 +127,13 @@ const Plan = () => {
 
                     {/*  */}
                 </div>
-                <div className='py-4 x-5 rounded-lg bg-[#F0F6FF] md:col-span-3'>
-                    <div className='flex gap-4 items-center justify-center'>
+                {/* <div className='py-4 rounded-xl bg-[#FAFBFF] mt-2 md:col-span-3'> */}
+                    <div className='flex gap-6 py-3 items-center bg-[#FAFBFF] justify-center mt-8 rounded-xl '>
                         <p className='text-primary-100 font-bold duration-200'>Monthly</p>
                         <Switch className='bg-[#02295A]' defaultChecked={toggleOn} onChange={onChange} />
                         <p className='font-bold text-[#9699ab] text-base duration-200'>Yearly</p>
                     </div>
-                </div>
+                {/* </div> */}
                 <div className="flex justify-between mt-8 md:mt-[146px]">
                     <button className="text-[#9699AB] text-base duration-200 hover:text-[#02295A]"><Link href="/">Go Back</Link></button>
                     <button className="bg-[#02295A] py-3 px-5 rounded-lg text-white ml-auto duration-[.15s] cursor-pointer">
